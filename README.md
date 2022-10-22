6aa32ac9e72be0a221449852d7ba41c7bcc9eba0
saripudin <arieptea@gmail.com>
2022-10-23 05:28:53 +0700

    lab2py

---
+++ README.md
@@ -0,0 +1,4 @@
+- Menggunakan fungsi input untuk mengambil nilai variable dari keyboard
+
+![image latihan](D:\lab2py\output-latihan3.PNG)
+#lab2py
---
+++ latihan1.py
@@ -0,0 +1,6 @@
+print("hello")
+print("saya sedang belajar python")
+#menampilkan tulisan 'Hello' di layar
+print("Hello")
+#menampilkan tulisan 'Saya sedang belajar pyton' di layar
+print("Saya sedang belajar python")
